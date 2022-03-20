da6d7fc64f0f70e4b03bd0e666ed2ac89f652405
Christine Nguyen <cnhnguyen59@gmail.com>
2022-03-20 15:41:28 -0400

    created put and delete routes for user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const { User, Thought } = require('../models');
 
 module.exports = {
     //gets all users
@@ -29,4 +29,30 @@ module.exports = {
           .then((dbUserData) => res.json(dbUserData))
           .catch((err) => res.status(500).json(err));
       },
+    //delete user
+    deleteUser(req, res) {
+      User.findOneAndDelete({_id:req.params.userId})
+        .then((user)=>{
+          !user 
+            ? res.status(404).json({message:'No user with that ID'})
+            : Thought.deleteMany({_id:{$in:user.thoughts}})
+        })
+        .then(() => res.json({message:'User and associated thoughts deleted!'}))
+        .catch((err)=> res.status(500).json(err))
+    },
+    updateUser(req, res) {
+      User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      )
+        .then((user)=>{
+          !user
+            ? res.status(400).json({message:'No user with that ID'})
+            : res.status(200).json(user)
+        })
+        .catch((err)=>{
+          if (err) res.status(500).json({message: 'Error updating user'})
+        })
+    }
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,12 +4,14 @@ const {
     getUsers,
     getSingleUser,
     createUser,
+    deleteUser,
+    updateUser
 } = require('../../controllers/userController')
 
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
-router.route('/:userId').get(getSingleUser);
+router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser)
 
 module.exports = router;
