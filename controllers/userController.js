782cda7769811a7bf158f7821a9fe9b22273346f
Christine Nguyen <cnhnguyen59@gmail.com>
2022-03-21 01:20:55 -0400

    fixed delete user route to also delete associated thoughts

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,12 @@ const { User, Thought } = require('../models');
 
 module.exports = {
     //gets all users
-    getUsers(req,res) {
+    getUsers(req, res) {
+      User.find()
+        .then((users) => res.json(users))
+        .catch((err) => res.status(500).json({message:'Error getting all users'}));
+    }
+    /* getUsers(req,res) {
         User.find({}, (err, result)=>{
           if(result){
             res.status(200).json(result);
@@ -10,7 +15,7 @@ module.exports = {
             console.log('Error grabbing all users')
           }
         })
-     },
+     } */,
     //gets single user
     getSingleUser(req,res){
         User.findOne({_id:req.params.userId})
@@ -24,22 +29,33 @@ module.exports = {
     },
     //creates new user
     createUser(req, res) {
-        console.log(req.body)
         User.create(req.body)
           .then((dbUserData) => res.json(dbUserData))
           .catch((err) => res.status(500).json(err));
       },
-    //delete user
+    //delete user and associated thoughts
+   /*  deleteUser(req, res) {
+      User.findOneAndDelete({ _id: req.params.userId })
+        .then((user) =>
+          !user
+            ? res.status(404).json({ message: 'No user with that ID' })
+            : Thought.deleteMany({ _id: { $in: user.thoughts } })
+        )
+        .then(() => res.json({ message: 'User and associated thoughts deleted!' }))
+        .catch((err) => res.status(500).json(err));
+    },
+     */
     deleteUser(req, res) {
       User.findOneAndDelete({_id:req.params.userId})
-        .then((user)=>{
+        .then((user)=>
           !user 
             ? res.status(404).json({message:'No user with that ID'})
-            : Thought.deleteMany({_id:{$in:user.thoughts}})
-        })
+            : Thought.deleteMany({ _id: { $in:user.thoughts } })
+        )
         .then(() => res.json({message:'User and associated thoughts deleted!'}))
         .catch((err)=> res.status(500).json(err))
     },
+    //update user data
     updateUser(req, res) {
       User.findOneAndUpdate(
         { _id: req.params.userId },
